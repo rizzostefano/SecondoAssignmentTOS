d69c619444b53205e8dc8ceeb32c94a128a272be
rizzostefano <rstefano99@gmail.com>
2020-12-08 12:04:11 +0100

    close #5 : aggiunta commissione

--- src/main/java/it/unipd/tos/business/Bill.java
+++ src/main/java/it/unipd/tos/business/Bill.java
@@ -1,25 +1,28 @@
+////////////////////////////////////////////////////////////////////
+// STEFANO RIZZO 1193464
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos.business;
-import it.unipd.tos.business.TakeAwayBill;
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 import it.unipd.tos.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class Bill {
 
-    private List<MenuItem> itemList;
-    private User user;
+    public static final double
+            IMPORTO_MINIMO_SENZA_COMMISSIONE = 10.00;
 
-    public  Bill(List<MenuItem> l, User u) {
-        itemList = new ArrayList<MenuItem>(l);
-        user = new User(u);
-    }
-    public static double getOrderPrice(List<MenuItem> itemsOrdered, User user) throws RestaurantBillException {
+    public static double getOrderPrice(List<MenuItem> itemsOrdered,
+        User user) throws RestaurantBillException {
         double sum=0;
         for (MenuItem item : itemsOrdered) {
             sum+=item.getPrice();
+        }
+        if (sum <= IMPORTO_MINIMO_SENZA_COMMISSIONE) {
+            sum += 0.50;
         }
         return sum;
     }
--- src/test/java/it/unipd/tos/BillTest.java
+++ src/test/java/it/unipd/tos/BillTest.java
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////
+// STEFANO RIZZO 1193464
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.tos;
 
 import static org.junit.Assert.assertEquals;
@@ -24,20 +28,32 @@ public class BillTest {
     m1 = new MenuItem("BUDINO", "Pinguino", 5.00),
     m2 = new MenuItem("BUDINO", "Biancaneve", 7.50),
     m3 = new MenuItem("GELATO", "Coppa Nafta", 3.00),
-    m4 = new MenuItem("GELATO", "Banana Split", 6.30),
+    m4 = new MenuItem("GELATO", "Banana Split", 6.50),
     m5 = new MenuItem("BIBITA", "Coca Cola", 2.50),
     m6 = new MenuItem("BIBITA", "Fanta", 2.00);
 
     @Test
-    public void getOrderPrice_ValidValues_NoDiscount() {
+    public void getOrderPrice_ValidValues_NoDiscount_NoCommission() {
+        List<MenuItem> l1 = new LinkedList<>();
+        l1.add(m1);
+        l1.add(m4);
+        l1.add(m6);
+        try {
+            assertEquals(13.50, Bill.getOrderPrice(l1, U1), 0.01);
+        }catch (RestaurantBillException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getOrderPrice_ValidValues_NoDiscount_WithCommission() {
         List<MenuItem> l1 = new LinkedList<>();
         l1.add(m1);
         l1.add(m6);
         try {
-            assertEquals(7.00, Bill.getOrderPrice(l1, U1), 0.01);
+            assertEquals(7.50, Bill.getOrderPrice(l1, U1), 0.01);
         }catch (RestaurantBillException e) {
             e.printStackTrace();
         }
-
     }
 }
